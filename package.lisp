e7ee82416b4c971df9b675c36915658691bf4d43
Tyler Lee <wtleeiv@gmail.com>
2017-05-22 16:31:05 -0700

    cull exports

--- package.lisp
+++ package.lisp
@@ -2,20 +2,5 @@
 
 (defpackage #:lww
   (:use #:cl)
-  (:export #:*stream*
-           #:write-app
-           #:make-page)
-  ;; (:import-from #:uiop/filesystem
-  ;;               #:directory-files
-  ;;               #:subdirectories
-  ;;               #:file-exists-p)
-  ;; (:import-from #:cl-ppcre
-  ;;               #:scan
-  ;;               #:regex-replace)
-  ;; (:import-from #:spinneret
-  ;;               #:*html*)
-  ;; (:import-from #:lex
-  ;;               #:echo
-  ;;               #:dotfile-p)
-  )
+  (:export #:write-app))
 
