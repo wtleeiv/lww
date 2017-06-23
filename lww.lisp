4bca55e961c4213fe2d30803d13db84aac631467
Tyler Lee <wtleeiv@gmail.com>
2017-06-23 11:38:29 -0700

    skip dot-dirs

--- lww.lisp
+++ lww.lisp
@@ -5,9 +5,20 @@
 (defvar *file-write-regex* "(.+)_(\\w{2,4})\\.lisp")
 (defvar *directory-end-slash-regex* "(.*)(\\w+)$")
 
+(defun dotdir-p (dir)
+  (flet ((check-dotdir (dir-path)
+           (let* ((parent-path (uiop/pathname:pathname-parent-directory-pathname dir-path))
+                 (dir-name (namestring (uiop/pathname:enough-pathname dir-path parent-path)))
+                 (first-char (char dir-name 0)))
+             (char= #\. first-char))))
+    (if (pathnamep dir)
+        (check-dotdir dir)
+        (check-dotdir (pathname dir)))))
+
 (defun dotfile-p (file)
   (let* ((filename (file-namestring file))
          (first-char (char filename 0)))
+    (princ (char= #\. first-char))
     (char= #\. first-char)))
 
 (defun unsaved-warning (dotfile)
@@ -42,11 +53,13 @@
           (write-web lisp-file web-file rewrite)))))
 
 (defun write-files (dir force)
-  (let ((load-file (uiop/pathname:merge-pathnames* (pathname dir) (make-pathname :name "common" :type "lisp"))))
-    (when (uiop/filesystem:file-exists-p load-file)
-      (load load-file)))
-  (mapcar (lambda (x) (write-file x force)) (uiop/filesystem:directory-files dir "*.lisp"))
-  (mapcar (lambda (x) (write-files x force)) (uiop/filesystem:subdirectories dir)))
+  (unless (dotdir-p dir)
+    (break)
+    (let ((load-file (uiop/pathname:merge-pathnames* (pathname dir) (make-pathname :name "common" :type "lisp"))))
+      (when (uiop/filesystem:file-exists-p load-file)
+        (load load-file)))
+    (mapcar (lambda (x) (write-file x force)) (uiop/filesystem:directory-files dir "*.lisp"))
+    (mapcar (lambda (x) (write-files x force)) (uiop/filesystem:subdirectories dir))))
 
 ;; run this
 (defun write-app (dir &optional (force nil))
